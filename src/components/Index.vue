f2c15cd7ab4da9083840c8cc5ebbc49726b7c58c
Eden Ramoneda <edenlee143@gmail.com>
2020-03-22 19:08:14 -0700

    configured adminlte

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,20 +1,74 @@
 <template>
-  <div class="wrapper">
-      <div class="hold-transition layout-top-nav">
-        <nav class="main-header navbar navbar-expand navbar-dark navbar-info">
+  <div class="hold-transition layout-top-nav">
+    <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-info">
           <div class="container">
-            <a href="index3.html" class="navbar-brand">
-              <img src="" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-                  style="opacity: .8">
-              <span class="brand-text font-weight-light">AdminLTE 3</span>
-            </a>
+              <center><h2 class="text-white">COVID-19 Monitoring</h2></center>
           </div>
         </nav>
-      </div>
+
+        <div class="content-wrapper">
+          <div class="content">
+            <div class="container bg-white">
+              <div class="container-fluid">
+                <div class="row">
+                  <div class="col-lg-3 col-6 mt-3">
+                    <!-- small box -->
+                    <div class="small-box bg-info">
+                      <div class="inner">
+                        <h3>150</h3>
+
+                        <p>New Orders</p>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-bag"></i>
+                      </div>
+                      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-3 col-6 mt-3">
+                    <!-- small box -->
+                    <div class="small-box bg-success">
+                      <div class="inner">
+                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+
+                        <p>Bounce Rate</p>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-stats-bars"></i>
+                      </div>
+                      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                  <div class="col-lg-3 col-6 mt-3">
+                    <!-- small box -->
+                    <div class="small-box bg-warning">
+                      <div class="inner">
+                        <h3>44</h3>
+
+                        <p>User Registrations</p>
+                      </div>
+                      <div class="icon">
+                        <i class="ion ion-person-add"></i>
+                      </div>
+                      <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
   </div>
 </template>
 
+<style scoped>
+@import 'admin-lte/dist/css/adminlte.css';
+</style>
+
 <script>
+
 export default {
   name: 'Index',
   data () {
--- src/router/index.js
+++ src/router/index.js
@@ -6,9 +6,10 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import money from 'v-money'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueAdminLte from 'vue-adminlte3';
 
-Vue.use(VueAdminLte, {/* options */});
+//import VueAdminLte from '@cookieseater/vue-adminlte3'
+
+//Vue.use(VueAdminLte, {/* options */});
 Vue.use(money, {precision: 4})
 Vue.use(Router)
 Vue.use(BootstrapVue)
