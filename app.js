c7538e86361bd12ef7525ae7afb4d4047877a502
EdenRamoneda <edenlee143@gmail.com>
2020-03-26 14:02:14 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors);
+app.use(express.static(path.join(__dirname, 'dist'))); // set the default directory 
+app.set('port', process.env.PORT || 3000); // we use process.env.PORT so that it would work on heroku environment, if null then it will fallback to 3000
+
+const server = app.listen(app.get('port'), function() { 
+  console.log('listening on port ', server.address().port); 
+});
